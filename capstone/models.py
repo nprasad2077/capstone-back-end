5f4ef0235a5084aa516e8117baabe3bca1863d08
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-12-27 14:22:32 -0600

    migration complete

--- capstone/models.py
+++ capstone/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+
+class Astronaut(models.Model):
+    name = models.CharField(max_length=100)
+    favorite_planet = models.CharField(max_length=100)
+    photo_url = models.TextField()
+    planets = ArrayField(models.CharField(max_length=100, blank=True))
+
+
+    def __str__(self):
+        return self.name
+
