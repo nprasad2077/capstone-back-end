36a6fe2f9c16b808e6d0ddf2423fee027ed63d99
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-12-27 11:31:09 -0600

    django views

--- capstone_django/settings.py
+++ capstone_django/settings.py
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'capstone_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'capstone',
+        'USER': 'capstoneuser',
+        'PASSWORD': 'capstone',
+        'HOST': 'localhost'
     }
 }
+
 
 
 # Password validation
---
+++ settings.sql
@@ -0,0 +1,4 @@
+-- settings.sql
+CREATE DATABASE capstone;
+CREATE USER capstoneuser WITH PASSWORD 'capstone';
+GRANT ALL PRIVILEGES ON DATABASE capstone TO capstoneuser;
