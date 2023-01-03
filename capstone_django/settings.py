6960b7e219e99f5320899be3684cc7e9e2d3f4cd
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-01-03 13:25:53 -0600

    localhost works with .env string

--- capstone_django/settings.py
+++ capstone_django/settings.py
@@ -97,21 +97,9 @@ WSGI_APPLICATION = 'capstone_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#   'default': dj_database_url.config(conn_max_age=600)
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'capstone',
-        'USER': 'ravi',
-        'PASSWORD': 'houston',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+  'default': dj_database_url.config(conn_max_age=600)
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
